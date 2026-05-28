b4d87de0edd6ee8aab06b0cb335253a4611efd81
Joerg Koerner DK8DE <joerg@dk8.de>
2026-05-28 23:02:16 +0200

    Update version to 1.9.3 in version.py for release

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 """Versions- und Metadaten für den FT-991/A Audiomanager."""
 
 APP_NAME = "FT-991/A Audiomanager"
-APP_VERSION = "1.9.2"
+APP_VERSION = "1.9.3"
 APP_DATE = "25.05.2026"
 APP_AUTHOR = "Jörg Körner DK8DE"
 APP_COPYRIGHT = "2026 Jörg Körner DK8DE"
